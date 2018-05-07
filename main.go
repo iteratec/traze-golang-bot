74acdc6fba061ca13de1edf775b5fa96929c5ab1
Max Berndt <max.berndt@iteratec.de>
2018-05-07 09:11:53 +0200

    Testing ImagePullPolicy: always

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
     logging.InitLogging()
-    logging.Log.Notice("This is a traze-go-bot. Go beat me :)")
+    logging.Log.Notice("This is traze-go-bot. Go beat me!")
 
     bot.NewBot()
     //bot.NewDebug()
